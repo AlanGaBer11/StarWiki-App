2f97dba873a7f73b2975285cd9d3e52b77cb0f86
alanGarcia <220258@utags.edu.mx>
2025-10-13 17:54:25 -0600

    Feat (Router): Implementación de enrutamiento (Router)

---
+++ Frontend/src/router/AppRouter.tsx
@@ -0,0 +1,24 @@
+import { IonReactRouter } from "@ionic/react-router";
+import { IonRouterOutlet } from "@ionic/react";
+import { Redirect, Route } from "react-router-dom";
+import Menu from "../components/Menu";
+import Page from "../pages/Page";
+
+const AppRouter: React.FC = () => {
+  return (
+    <IonReactRouter>
+        <Menu/>
+      <IonRouterOutlet id="main">
+        {/* Rutas */}
+        <Route path="/" exact={true}> {/* Ruta por defecto */}
+          <Redirect to="/folder/Inbox" />
+        </Route>
+        <Route path="/folder/:name" exact={true}> {/* Ruta dinamica */}
+          <Page />
+        </Route>
+      </IonRouterOutlet>
+    </IonReactRouter>
+  );
+};
+
+export default AppRouter;
