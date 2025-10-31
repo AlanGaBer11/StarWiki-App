bbe3411770fab8b47f4201f5548a46a12f30ba14
alanGarcia <220258@utags.edu.mx>
2025-10-31 12:05:58 -0600

    Feat (Android): Implementacion de splashScreen y logos

--- Frontend/capacitor.config.ts
+++ Frontend/capacitor.config.ts
@@ -1,9 +1,26 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "io.ionic.starter",
+  appId: "com.starwiki.app",
   appName: "StarWiki",
   webDir: "dist",
+  plugins: {
+    splashscreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#094753",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: false,
+      splashImmersive: false,
+      layoutName: "launc_screen",
+      useDialog: false,
+    },
+  },
 };
 
 export default config;
